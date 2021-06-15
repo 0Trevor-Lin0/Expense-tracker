42eed72623f2550f4a868a2c76223be04f3eb55b
Trevor Lin <trevor0966@gmail.com>
2021-06-15 16:18:43 +0800

    feat: 設定總路由器和第一個路由模組

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
+const routes = require('./routes/index')
 // 建立mongoose 與 mongodb連結
 // 引入路由器
 
@@ -10,9 +11,7 @@ const port = process.env.PORT || 3000
 app.engine('handlebars', exphbs({ defaultLayout: 'main', extname: '.handlebars' }))
 app.set('view engine', 'handlebars')
 
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
+app.use(routes)
 
 app.listen(port, () => {
   console.log(`the app is running on localhost:${port}`)

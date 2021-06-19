e081c1d8b123148d8316cd6177108b8eff58b5dd
Trevor Lin <trevor0966@gmail.com>
2021-06-19 23:16:04 +0800

    feat: 渲染首頁畫面 & 日期資料調整呈現型態

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const Record = require('../../models/record')
+
 router.get('/', (req, res) => {
-  res.render('index')
+  Record.find()
+    .lean()
+    .sort('_id')
+    .then(records => {
+      records.forEach(record => {
+        record.date = record.date.toLocaleDateString()
+      })
+      res.render('index', { records })
+    })
+    .catch(error => console.log(error))
 })
 
 module.exports = router

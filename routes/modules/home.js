4a5090f11d669af322844c3026d4dd69c9802d57
Trevor Lin <trevor0966@gmail.com>
2021-06-20 20:16:56 +0800

    feat: add totalAmount & get category data to match record data to render icon

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const router = express.Router()
-
+const Category = require('../../models/category')
 const Record = require('../../models/record')
+let totalAmount = 0
 
 router.get('/', (req, res) => {
   Record.find()
     .lean()
     .sort('_id')
     .then(records => {
-      records.forEach(record => {
-        record.date = record.date.toLocaleDateString()
-      })
-      res.render('index', { records })
+      Category.find()
+        .lean()
+        .then(categories => {
+          records.forEach(record => {
+            categories.find(category => {
+              if (record.category === category.name) {
+                record.icon = category.icon
+              }
+            })
+            record.date = record.date.toDateString() // 改變取出date呈現方式
+            totalAmount += record.amount
+          })
+          res.render('index', { records, totalAmount })
+        })
     })
+
     .catch(error => console.log(error))
 })
 
